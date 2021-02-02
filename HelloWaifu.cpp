df442d1e5ce8c63ad484981f22d18cd61da7984e
Ole Nyheim <ole.t.nyheim@gmail.com>
2021-02-02 15:20:06 +0100

    shitfuck

---
+++ HelloWaifu.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+        cout << "Hello Waifu!\n";
+        int age;
+        cout << "Hvor gammel er waifuen din?: ";
+        cin >> age;
+        if (age < 18)
+        {
+            cout << "Waifuen din er ikke gammel nok.\n";
+        }
+        else
+        {
+            cout << "Waifuen din er gammel nok.\n";
+        }
+        cout << "\n";
+	}
+}
